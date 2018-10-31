936005fe7a2b1244f33b2df64f0db7c0f540e278
Agebull <Agebull@qq.com>
2018-10-31 18:18:52 +0800

    新增扩展支持文件

---
+++ src/Rpc.Abstractions/Context/IGlobalContextHelper.cs
@@ -0,0 +1,24 @@
+using Agebull.Common.OAuth;
+
+namespace Agebull.Common
+{
+    /// <summary>
+    /// 上下文的辅助对象
+    /// </summary>
+    public interface IGlobalContextHelper
+    {
+        /// <summary>
+        /// 生成一个用户对象
+        /// </summary>
+        /// <param name="type">0 无要求即只构造，1 构造一个匿名用户，2 构造一个系统用户</param>
+        /// <returns></returns>
+        ILoginUserInfo CreateUserObject(int type);
+
+
+        /// <summary>
+        /// 生成一个组织对象
+        /// </summary>
+        /// <returns></returns>
+        IOrganizational CreateOrganizationalObject();
+    }
+}
